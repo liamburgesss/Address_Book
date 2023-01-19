a9ff4a0392848202268c1680d2893595253a198b
liamburgesss <liam.burgess@labs.epiuse.com>
2023-01-19 08:22:47 +0200

    ✨

--- webapp/controller/Detailed.controller.js
+++ webapp/controller/Detailed.controller.js
@@ -1,21 +1,34 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("pb.controller.Detailed", {
-    onInit: function () {
-      var oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("detailed")
-        .attachPatternMatched(this._onObjectMatched, this);
-    },
-    _onObjectMatched: function (oEvent) {
-      this.getView().bindElement({
-        path:
-          "/" +
-          window.decodeURIComponent(
-            oEvent.getParameter("arguments").contact
-          ),
-        model: "contacts",
-      });
-    },
-  });
-});
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", 
+  "sap/ui/core/routing/History"],
+  function (Controller, History) {
+    "use strict";
+    return Controller.extend("pb.controller.Detailed", {
+      onInit: function () {
+        var oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("detailed")
+          .attachPatternMatched(this._onObjectMatched, this);
+      },
+      _onObjectMatched: function (oEvent) {
+        this.getView().bindElement({
+          path:
+            "/" +
+            window.decodeURIComponent(oEvent.getParameter("arguments").contact),
+          model: "contacts",
+        });
+      },
+      goHome: function () {
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          var oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("phonebook", {}, true);
+        }
+      },
+    });
+  }
+);
