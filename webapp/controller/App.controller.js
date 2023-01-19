973abd390caf3cdb8650043374be49f72308c26a
liamburgesss <liam.burgess@labs.epiuse.com>
2023-01-19 13:01:21 +0200

    ✨

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,58 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
- ], function (Controller) {
-    "use strict";
-    return Controller.extend("pb.controller.App", {
-        goToDetailed : function (oEvent) {
-            var oItem = oEvent.getSource();
-			var oRouter = this.getOwnerComponent().getRouter();
-			oRouter.navTo("detailed", {
-				contact: window.encodeURIComponent(oItem.getBindingContext("contacts").getPath().substr(1))
-			});
-        }
-    });
- });
+sap.ui.define(["sap/ui/core/mvc/Controller",
+"sap/ui/model/json/JSONModel"], function (Controller) {
+  "use strict";
+  return Controller.extend("pb.controller.App", {
+    goToDetailed: function (oEvent,JSONModel) {
+      var oItem = oEvent.getSource();
+      var oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo("detailed", {
+        contact: window.encodeURIComponent(
+          oItem.getBindingContext("contacts").getPath().substr(1)
+        ),
+      });
+    },
+    onBeforeRendering: function (oEvent) {
+      setTimeout(
+        function () {
+          var provinces = [];
+          var address = [];
+
+          var oList = Object.values(
+            this.getView().getModel("contacts").getProperty("/")
+          );
+
+          provinces = [];
+          address = [];
+
+          for (var i = 0; i < oList.length; i++) {
+            provinces.push(oList[i].region);
+            address.push(oList[i].address);
+          }
+          provinces = provinces.filter(
+            (item, index) => provinces.indexOf(item) === index
+          );
+          address = address.filter(
+            (item, index) => address.indexOf(item) === index
+          );
+
+          var jsonAddress = [];
+          for (var i = 0; i < address.length; i++) {
+            jsonAddress.push({ address: address[i] });
+          }
+
+          var jsonProvince = [];
+          for (var i = 0; i < provinces.length; i++) {
+            jsonProvince.push({ province: provinces[i] });
+          }
+
+          var oModel = new JSONModel(jsonProvince);
+          this.getView().setModel(oModel);
+
+          var oModel = new JSONModel(jsonAddress);
+          this.getView().setModel(oModel);
+        }.bind(this),
+        500
+      );
+    },
+  });
+});
